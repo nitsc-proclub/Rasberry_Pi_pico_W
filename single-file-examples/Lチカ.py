4cf6cc0a8c2735c04e276b85b5d2a6f76a828ce6
Fukutti-WanaharaJassie <yoshi.saku.ko9189@gmail.com>
2025-08-03 20:57:52 +0900

    Update Lチカ.py

--- single-file-examples/Lチカ.py
+++ single-file-examples/Lチカ.py
@@ -6,5 +6,7 @@ led1 = LED(5)                   # GP5にLED接続
 while True:                     # ずっと繰り返す
     led1.on()                   # 点灯
     sleep(0.5)                  # 0.5秒待つ
+    
     led1.off()                  # 消灯
     sleep(0.5)                  # 0.5秒待つ
+

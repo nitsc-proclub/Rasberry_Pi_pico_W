e9541570d8d7ec696abd667cc614fd3bb366fc8c
hachi-mori <107915908+hachi-mori@users.noreply.github.com>
2025-08-15 14:49:53 +0900

    角度を修正

--- single-file-examples/サーボモーター.py
+++ single-file-examples/サーボモーター.py
@@ -4,7 +4,7 @@ from time import sleep       # 待ち時間
 servo = Servo(0, None, 0.0005, 0.0024)  # GP0に接続して初期化
 
 while True:                  # ずっと繰り返す
-    servo.value = 0 / 90   # サーボを0°に動かす
+    servo.value = 45 / 90   # サーボを45°に動かす
     sleep(0.5)              # 0.5秒待つ
-    servo.value = 90 / 90  # サーボを90°に動かす
+    servo.value = 135 / 90  # サーボを135°に動かす
     sleep(0.5)              # 0.5秒待つ
